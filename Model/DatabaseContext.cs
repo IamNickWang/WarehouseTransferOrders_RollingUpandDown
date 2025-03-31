8ccdb5819af2124dd318f25f8348e2693556325f
Nick Wang <nickwang33@hotmail.com>
2025-03-31 13:36:17 -0700

    Add files via upload

---
+++ Model/DatabaseContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using System.Data;
+using Dapper;
+
+namespace WarehouseTransferOrders.Model
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+        {
+        }
+
+        public DbSet<erpTransferOrder> erpTransferOrder { get; set; }
+        public async Task<List<erpTransferOrder>> LoadData<erptransferOrder>(string sql, string conn)
+        {
+
+            using (IDbConnection connection = new SqlConnection(conn))
+            {
+                var data = await connection.QueryAsync<erpTransferOrder>(sql);
+
+                return data.ToList();
+            }
+
+        }
+
+    }
+}
---
+++ Model/erpTransferOrder.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WarehouseTransferOrders.Model
+{
+    public class erpTransferOrder
+    {
+        public string recordType { get; set; }
+        [Key]
+        public Int64? internalId { get; set; }
+        public DateTime? tranDate { get; set; }
+        public string type { get; set; }
+        public string documentNumber { get; set; }
+        public string status { get; set; }
+        public string location { get; set; }
+        public string transferLocation { get; set; }
+        public string createdBy { get; set; }
+        public string deliveryInstruction { get; set; }
+    }
+}
